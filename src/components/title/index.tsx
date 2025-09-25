675786ef49f482a4806920b154f63db0f5b2dc9f
M-lakshan <emlakshan92@gmail.com>
2025-09-25 13:45:07 +0530

    param renamed

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -11,25 +11,25 @@ export const Title = ({
   context,
   className,
   expanders,
-  divisable,
+  dividable,
 }: LocalContainerProps & {
   rootElementAlt: "h6" | "h5" | "h4" | "h3" | "h2" | "h1" | "p";
   context: string;
   expanders?: boolean;
-  divisable?: boolean;
+  dividable?: boolean;
 }) => {
   const cmpStyles = SetComponentStyles({ className, styleObj });
   const RootElement: React.ElementType = rootElementAlt || "h3";
 
   return (
     <RootElement {...(cmpID && { id: cmpID })} {...(cmpStyles && { className: cmpStyles })}>
-      {expanders && <span className={`expander${divisable ? "border-expand" : ""}`}>&nbsp;</span>}
+      {expanders && <span className={`expander${dividable ? "border-expand" : ""}`}>&nbsp;</span>}
       {!children ? (
         <span className={`context ${expanders ? "speard-out" : ""}`}>{context}</span>
       ) : (
         <>{children}</>
       )}
-      {expanders && <span className={`expander${divisable ? "border-expand" : ""}`}>&nbsp;</span>}
+      {expanders && <span className={`expander${dividable ? "border-expand" : ""}`}>&nbsp;</span>}
     </RootElement>
   );
 };
